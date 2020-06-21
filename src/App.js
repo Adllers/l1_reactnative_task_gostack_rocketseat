4a577fd532b7d16cca817305183713bae6efab1f
Adller Simas <adller.eel.ufsc@gmail.com>
2020-06-21 18:52:48 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
-import {
-  SafeAreaView,
-  View,
-  FlatList,
-  Text,
-  StatusBar,
-  StyleSheet,
-  TouchableOpacity,
-} from "react-native";
+import { SafeAreaView, View, FlatList, Text, StatusBar, StyleSheet, TouchableOpacity} from 'react-native';
 
 export default function App() {
 	
 	const [repositories, setRepositories] = useState([]);
 
 	useEffect(() => {
-		api.get('repositories').then(response => {
+		api.get('/repositories').then(response => {
 			setRepositories(response.data);
 		});
 	}, []);
 
-	
+	async function handleDeleteRepository(id) {
+		await api.delete(`repositories/${id}`);
+
+		const newRepository = repositories.filter(
+			repository => repository.id !== id
+		);
+		setRepositories(newRepository);
+	}
+
 	async function handleLikeRepository(id) {
 		await api.post(`repositories/${id}/like`).then(response => {
 			const repositoryLiked = response.data;
@@ -32,21 +33,39 @@ export default function App() {
 		});
 	}
 
+	async function handleAddProject() {
+		const response = await api.post('/repositories', {
+			title: `Repository: #${Date.now()}`,
+			url: 'http://...',
+			techs: ['NodeJS'],
+		});
+
+		const newRepository = response.data;
+
+		setRepositories([...repositories, newRepository]);
+	}
+
 	return (
 		<>
 			<StatusBar barStyle='light-content' backgroundColor='#7159c1' />
 			<SafeAreaView style={styles.container}>
-			
-      	<FlatList
+				<FlatList
 					data={repositories}
 					key={repository => repository.id}
 					renderItem={({ item: repository }) => {
-					return (
-						<>
+						return (
+							<>
 							<View style={styles.repositoryContainer}>
 								<View style={styles.header}>
 									<Text style={styles.repository}>{repository.title}</Text>
 
+									<TouchableOpacity
+										style={styles.buttonDelete}
+										activeOpacity={0.6}
+										onPress={() => handleDeleteRepository(repository.id)}
+									>
+										<Text style={styles.buttonDeleteText}>X</Text>
+									</TouchableOpacity>
 								</View>
 
 								<View style={styles.techsContainer}>
@@ -54,24 +73,24 @@ export default function App() {
 										<Text key={tech} style={styles.tech}>
 											{tech}
 										</Text>
-									))}
+										))}
 								</View>
 
 								<View style={styles.likesContainer}>
 									<Text
 										style={styles.likeText}
-											
-										testID={`repository-likes-${repository.id}`}
+											// Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
+											testID={`repository-likes-${repository.id}`}
 									>
-											{`${repository.likes} likes`}
+										{`${repository.likes} likes`}
 									</Text>
 								</View>
 
 								<TouchableOpacity
-									activeOpacity={0.8}
+									activeOpacity={0.6}
 									style={styles.button}
 									onPress={() => handleLikeRepository(repository.id)}
-										
+								// Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
 									testID={`like-button-${repository.id}`}
 								>
 									<Text style={styles.buttonText}>Curtir</Text>
@@ -81,7 +100,13 @@ export default function App() {
 						);
 					}}
 				/>
-				
+				<TouchableOpacity
+					style={styles.buttonAdd}
+					activeOpacity={0.6}
+					onPress={handleAddProject}
+				>
+					<Text style={styles.buttonAddText}>Adicionar projeto</Text>
+				</TouchableOpacity>
 			</SafeAreaView>
 		</>
 	);
